042dbde547abf8573fda2e78f3f55bb0318666f1
Homdev <homdev44@gmail.com>
2024-11-05 12:46:38 +0100

    Fix gunicorn

--- app.py
+++ app.py
@@ -1,4 +1,30 @@
-from app import app
+import os
+from flask import Flask
+from flask_cors import CORS
+import logging
+from app.utils.model_loader import download_model
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+app = Flask(__name__)
+
+# Créer les dossiers de travail
+for folder in ['static/upload', 'static/processed', 'models']:
+    os.makedirs(folder, exist_ok=True)
+
+# Vérifiez si le modèle est déjà présent avant de le télécharger
+model_path = os.path.join('models', 'u2net.pth')
+if not os.path.exists(model_path):
+    try:
+        download_model()
+    except Exception as e:
+        logger.error(f"Erreur fatale lors du téléchargement du modèle: {str(e)}")
+        raise
+
+# CORS configuration
+CORS(app, resources={r"/*": {"origins": ["http://localhost:3000", "https://dashboard-podologie.netlify.app"]}})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
