49e0bd43fb1d695cb6c5ee0a336c60bc4b30f7b2
homdev <44805789+homdev@users.noreply.github.com>
2024-11-27 11:48:52 +0100

    Update __init__.py

--- app/utils/__init__.py
+++ app/utils/__init__.py
@@ -1,10 +1,11 @@
-from .image_processing import remove_background, place_on_a4_canvas, create_a4_image_with_scale, verify_dimensions, split_feet_improved, create_adaptive_mask, calculate_foot_masses, save_image, isolate_foot, ImageProcessingError, clean_foot_image, crop_to_content, adaptive_contrast_brightness, enhance_mask_edges, refine_mask, refine_and_clean_mask, correct_color_cast, process_and_save_image, find_separation_valley
+from .image_processing import remove_background, create_a4_image_with_scale, clean_transparency, verify_dimensions, split_feet_improved, create_adaptive_mask, calculate_foot_masses, save_image, isolate_foot, ImageProcessingError, clean_foot_image, crop_to_content, adaptive_contrast_brightness, enhance_mask_edges, refine_mask, refine_and_clean_mask, correct_color_cast, process_and_save_image, find_separation_valley
 from .model_loader import download_model
 
 __all__ = [
     'remove_background',
     'split_feet_improved',
     'save_image',
+    'process_and_save_image',
     'ImageProcessingError',
     'download_model',
     'clean_foot_image',
@@ -15,14 +16,10 @@ __all__ = [
     'refine_mask',
     'refine_and_clean_mask',
     'correct_color_cast',
-    'process_and_save_image',
     'isolate_foot',
     'create_adaptive_mask',
     'calculate_foot_masses',
     'find_separation_valley',
-    'place_on_a4_canvas',
     'verify_dimensions',
-    'create_a4_image_with_scale'
-    
-    
+    'create_a4_image_with_scale',
 ]
